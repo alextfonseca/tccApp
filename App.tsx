d71a767b09228bf8f4c19d1bb1e710a0ae3fc1c4
Alex Teixeira da Fonseca <alexatf2014@gmail.com>
2022-07-07 21:09:55 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+
+import { Home } from './src/screens/Home'
+
+import {
+  useFonts,
+  Raleway_200ExtraLight,
+  Raleway_300Light,
+  Raleway_400Regular,
+  Raleway_500Medium,
+  Raleway_700Bold
+} from '@expo-google-fonts/raleway'
+import AppLoading from 'expo-app-loading'
+
+import { ThemeProvider } from 'styled-components/native'
+
+import { StatusBar } from 'expo-status-bar'
+import theme from './src/theme'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [fontsLoaded] = useFonts({
+    Raleway_200ExtraLight,
+    Raleway_300Light,
+    Raleway_400Regular,
+    Raleway_500Medium,
+    Raleway_700Bold
+  })
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <StatusBar backgroundColor="light" />
+      <Home />
+    </ThemeProvider>
+  )
+}
---
+++ src/theme/index.ts
@@ -0,0 +1,24 @@
+export default {
+  colors: {
+    green: '#87E043',
+    green100: '#409400',
+    green200: '#2A6100',
+
+    gray: '#808080',
+
+    black: '#000000',
+
+    white: '#ffffff',
+    lightGreen: '#E7F9D9',
+
+    background: '#FBFFF8'
+  },
+
+  fonts: {
+    extraLight: 'Raleway_200ExtraLight',
+    light: 'Raleway_300Light',
+    regular: 'Raleway_400Regular',
+    medium: 'Raleway_500Medium',
+    bold: 'Raleway_700Bold'
+  }
+}
